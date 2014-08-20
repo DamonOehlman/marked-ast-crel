21533a94e646ac32d06ea392b3ae1683eb3cdaf1
Damon Oehlman <damon.oehlman@gmail.com>
2014-08-20 23:23:06 +1000

    Updated examples

--- examples/complex.js
+++ examples/complex.js
@@ -1,10 +1,23 @@
 var fs = require('fs');
 var marked = require('marked-ast');
-var crel = require('..');
+var hljs = require('highlight.js');
+var qsa = require('fdom/qsa');
+var crel = require('crel');
+var markedCrel = require('..');
 var content = fs.readFileSync(__dirname + '/complex.md', 'utf8');
 
 function append(el) {
   document.body.appendChild(el);
 }
 
-marked.parse(content).map(crel).forEach(append);
+document.body.appendChild(crel('link', {
+  rel: 'stylesheet',
+  href: 'https://highlightjs.org/static/styles/default.css'
+}));
+
+marked.parse(content).map(markedCrel).forEach(append);
+
+qsa('pre code').forEach(function(el) {
+  hljs.highlightBlock(el);
+});
+
--- examples/simple.js
+++ examples/simple.js
@@ -1,8 +1,8 @@
 var marked = require('marked-ast');
-var crel = require('..');
+var markedCrel = require('..');
 
 function append(el) {
   document.body.appendChild(el);
 }
 
-marked.parse('Some **test** [content](http://www.google.com)').map(crel).forEach(append);
+marked.parse('Some **test** [content](http://www.google.com)').map(markedCrel).forEach(append);
